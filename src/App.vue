df47963a70222643cdaf1cb4d52dacd5fc64e0c6
hekaibo <97099722@qq.com>
2018-01-16 16:21:19 +0800

    https://github.com/hekb023/vue-music.git

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+
+  export default {
+    name: 'app',
+    components: {
+      MHeader,
+      Tab
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "common/stylus/variable"
+</style>
